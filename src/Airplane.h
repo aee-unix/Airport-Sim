f98e00e8992d2dad4ddab7484b088607acc7740a
geppettodivacin <ericdilmore@gmail.com>
2015-03-25 00:10:23 -0500

    Add Airplane class.

---
+++ src/Airplane.h
@@ -0,0 +1,28 @@
+// =====================================================================================
+// 
+//       Filename:  Airplane.h
+// 
+//    Description:  Header for Airplane class.
+// 
+//        Version:  1.0
+//        Created:  03/24/2015 08:23:26 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Eric Dilmore (), ericdilmore@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#ifndef AIRPLANE_H
+#define AIRPLANE_H
+
+class Airplane
+{
+    public:
+        int fuel;
+        int timeIn;
+        Airplane ( int fuel, int timeIn );
+}
+
+#endif
