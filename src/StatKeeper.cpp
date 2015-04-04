56633fe041e19b881db05ffbbc308ca0dea9a183
geppettodivacin <ericdilmore@gmail.com>
2015-04-04 02:25:56 -0500

    Add StatKeeper.cpp.

---
+++ src/StatKeeper.cpp
@@ -0,0 +1,70 @@
+// =====================================================================================
+// 
+//       Filename:  StatKeeper.cpp
+// 
+//    Description:  Keeps statistics about plans crashed, landed, and taken off.
+// 
+//        Version:  1.0
+//        Created:  04/04/2015 02:19:57 AM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Eric Dilmore (), ericdilmore@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#include "StatKeeper.h"
+
+int StatKeeper::getTotalTakeoffTime()
+{
+    // Blank body
+}
+int StatKeeper::getTotalLandingTime()
+{
+    // Blank body
+}
+int StatKeeper::getTakeoffs()
+{
+    // Blank body
+}
+int StatKeeper::getLandings()
+{
+    // Blank body
+}
+int StatKeeper::getCrashes()
+{
+    // Blank body
+}
+void StatKeeper::incrementTakeoffTime(int time)
+{
+    // Blank body
+}
+void StatKeeper::incrementLandingTime(int time)
+{
+    // Blank body
+}
+void StatKeeper::setTakeoffs()
+{
+    // Blank body
+}
+void StatKeeper::setLandings()
+{
+    // Blank body
+}
+void StatKeeper::setCrashes()
+{
+    // Blank body
+}
+void StatKeeper::incrementTime()
+{
+    // Blank body
+}
+void StatKeeper::printStats()
+{
+    // Blank body
+}
+int StatKeeper::getAverageTime()
+{
+    // Blank body
+}
