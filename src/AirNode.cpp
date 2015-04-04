b2f1e768e8e3ce0807b59c36ff9e4dedec874c96
geppettodivacin <ericdilmore@gmail.com>
2015-04-04 02:12:52 -0500

    Add AirNode.cpp.

---
+++ src/AirNode.cpp
@@ -0,0 +1,32 @@
+// =====================================================================================
+// 
+//       Filename:  AirNode.cpp
+// 
+//    Description:  Node for Queue holding airplanes
+// 
+//        Version:  1.0
+//        Created:  04/04/2015 01:44:47 AM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Eric Dilmore (), ericdilmore@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#include "AirNode.h"
+
+AirNode::getPlane()
+{
+    // Empty body
+}
+
+AirNode::getNextPlane()
+{
+    // Empty body
+}
+
+void AirPlane::setNextAirPlane(Airplane* p)
+{
+    // Empty body
+}
