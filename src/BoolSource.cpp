e3b722a0c8aae4a1b292e02f7708f85d6fc7b335
geppettodivacin <ericdilmore@gmail.com>
2015-04-04 02:15:38 -0500

    Add BoolSource.cpp.

---
+++ src/BoolSource.cpp
@@ -0,0 +1,22 @@
+// =====================================================================================
+// 
+//       Filename:  BoolSource.cpp
+// 
+//    Description:  Source of random boolean values.
+// 
+//        Version:  1.0
+//        Created:  04/04/2015 02:13:12 AM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Eric Dilmore (), ericdilmore@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#include "BoolSource.h"
+
+bool BoolSource::randBool()
+{
+    // Empty body
+}
