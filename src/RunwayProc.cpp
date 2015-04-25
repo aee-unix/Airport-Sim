ac51a93ab9c6bd7883fab3e62221b5cfce815c9a
geppettodivacin <ericdilmore@gmail.com>
2015-04-25 15:48:40 -0500

    Write shell of RunwayProc.

---
+++ src/RunwayProc.cpp
@@ -0,0 +1,34 @@
+// =====================================================================================
+// 
+//       Filename:  RunwayProc.cpp
+// 
+//    Description:  Class to spin a new process controlling a single runway.
+// 
+//        Version:  1.0
+//        Created:  04/25/2015 03:42:25 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Eric Dilmore (), ericdilmore@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#include <cstdlib>
+#include <iostream>
+#include "RunwayProc.h"
+
+using namespace std;
+
+RunwayProc::RunwayProc(Queue* queue, int tTime, int lTime,
+                       double prob, int fd)
+    : runway(queue, tTime, lTime, prob)
+    , sigFd(fd)
+{
+    // Empty body
+}
+
+void RunwayProc::run()
+{
+    // Empty body
+}
---
+++ src/RunwayProc.h
@@ -0,0 +1,33 @@
+// =====================================================================================
+// 
+//       Filename:  RunwayProc.h
+// 
+//    Description:  Header for RunwayProc.
+// 
+//        Version:  1.0
+//        Created:  04/25/2015 03:38:04 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Eric Dilmore (), ericdilmore@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#ifndef RUNWAYPROC_H
+#define RUNWAYPROC_H
+
+#include "Queue.h"
+#include "Runway.h" 
+
+class RunwayProc
+{
+    private:
+    Runway runway;
+    int sigFd;
+    public:
+    RunwayProc(Queue* queue, int tTime, int lTime, double prob, int fd);
+    void run();
+}
+
+#endif
